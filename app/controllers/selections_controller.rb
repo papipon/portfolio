b9fe3bf6a8c2763e60f956eea888855c64681231
papipon <daaiisk0616@gmail.com>
2019-07-16 07:55:25 +0000

    コントローラーアクション変更

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -2,35 +2,8 @@ class SelectionsController < ApplicationController
   def index
   end
 
-  def index01
-  	
+  def home
   end
 
-  def index02
-  end
-
-  def index03
-  end
-
-  def index04
-  end
-
-  def index05
-  end
-
-  def index06
-  end
-
-  def index07
-  end
-
-  def index08
-  end
-
-  def index09
-  end
-
-  def index10
-  end
 
 end
