f49de243c1a5522d2c7f103f6fd79e9a3ce8502c
papipon <daaiisk0616@gmail.com>
2019-07-21 08:03:22 +0000

    ランキング機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,17 @@ class ProductsController < ApplicationController
   def index
   	@q = Product.ransack(params[:q])
     @products = @q.result(distinct: true).page(params[:page])
-    
+    @all_ranks = Product.all.sort { |current_product, next_product| 
+									if current_product.reviews.first && next_product.reviews.first
+										next_product.reviews.average(:star) <=> current_product.reviews.average(:star)
+									elsif !next_product.reviews.first
+										-1
+									elsif !current_product.reviews.first
+										1
+									else
+										0
+									end
+									}.in_groups_of(5)[0]    
   end
 
   def show
@@ -14,3 +24,16 @@ class ProductsController < ApplicationController
 
 
 end
+
+
+Product.all.sort { |current_product, next_product| 
+	if current_product.reviews.first && next_product.reviews.first
+		current_product.reviews.average(:star) <=> next_product.reviews.average(:star)
+	elsif !current_product.reviews.first
+		1
+	elsif !next_product.reviews.first
+		-1
+	else
+		0
+	end
+	}.in_groups_of(5)[0]
