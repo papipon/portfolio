e0e6fe7d0a303467ed10513360853eae20514792
papipon <daaiisk0616@gmail.com>
2019-07-25 14:30:52 +0000

    selectionsコントローラー生成

---
+++ app/controllers/selections_controller.rb
@@ -0,0 +1,35 @@
+class SelectionsController < ApplicationController
+	def home
+		
+	end
+
+	def coice
+	end
+
+	def charge
+	end
+    
+    def colour
+    end
+
+    def comfort
+    end
+
+    def contry
+    end
+
+    def iphone
+    end
+
+    def music
+    end
+
+    def opition
+    end
+
+    def pick
+    end
+
+    def robust
+    end
+end
